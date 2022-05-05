4a2fd8617a790abaf7090182527b5a749fcdf7f5
Didem Gülşen Damlapınar <ddamlapinar@gmail.com>
2022-05-05 17:00:53 +0300

    feat : progress bar init

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,46 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const SIZES = {
+  small: {
+    height: '8px',
+    padding: 0,
+    borderRadius: '4px',
+  },
+  medium: { 
+    height: '12px',
+    padding: 0,
+    borderRadius: '4px'
+  },
+  large: {
+    height: '16px',
+    padding: '4px',
+    borderRadius: '8px',
+  }
+}
+
+const StyledProgressBar = styled.progress`
+  apperance: none;
+
+  &[value] {
+  width: 370px;
+  height: ${p => SIZES[p.size].height};
+  padding: ${p => SIZES[p.size].padding};
+    
+  ::-webkit-progress-bar {
+    background-color: ${COLORS.transparentGray15};
+    border-radius: ${p => SIZES[p.size].borderRadius};
+  }
+
+  ::-webkit-progress-value {
+    background-color: ${COLORS.primary};
+  }
+
+  `;
+const ProgressBar = ({ value, size,max }) => {
+  return (
+    <StyledProgressBar max={max} size={size} value={value}>{value}</StyledProgressBar>
+ );
 };
 
 export default ProgressBar;
